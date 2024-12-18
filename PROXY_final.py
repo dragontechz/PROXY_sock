d81e8b52f4793f655ebff247bcbcdbdf7a04099b
dragontechz <juleskono58@gmail.com>
2024-12-18 15:49:29 +0100

    Add files via upload

--- PROXY_final.py
+++ PROXY_final.py
@@ -1,5 +1,4 @@
 #coding:utf-8
-#coding:utf-8
 import threading
 import socket
 import select
@@ -8,50 +7,106 @@ IP = ""
 PORT = 9090
 default_HOST = '0.0.0.0:22'
 MSG = '@Dragontechz on telegram'
-RESPONSE = (f'HTTP/1.1 200 OK message: {MSG}\r\n\r\n').encode("utf-8")
-buffer = 1024
+RESPONSE = (f'HTTP/1.1 200 OK\r\nmessage: {MSG}\r\n\r\n').encode("utf-8")
+buffer = 1024*1024
 
-i = '127.0.0.1'
+
+def findhost(payload):
+     index = payload.find("Host:")
+     if index != -1:
+          debut = index + 5
+          fin = payload.find("\r\n",debut)
+          if fin != -1:
+               host = payload[debut:fin].strip()
+               return host
+          return None
+
 def sock():
      sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
      return sock
+
+def handle_data(data):
+     if len(data.decode()) == -1:
+          return None
+     data = data.decode()
+     print("handling request ...")
+     host = findhost(data)
+
+     if len(host) != -1:
+          print("request on host : {}".format(host))
+          sock2host = sock()
+          try:
+               if ":" in host:
+                    host_dest , port = host.split(":")
+                    print("connecting to {} on port : {}".format(host_dest,port))
+                    
+                    try:
+                         sock2host.connect((host_dest,int(port)))
+                         sock2host.send(data.encode('utf-8'))
+                         response = sock2host.recv(buffer)
+                         return response
+                    
+                    except socket.gaierror:
+                         print("cann't connect to host: {}".format(host))
+                         response = b'cannot connect to ' + host.encode('utf-8')
+                         return response
+                    
+               else:
+                    try:
+                         sock2host.connect((host,80))
+                         sock2host.send(data.encode('utf-8'))
+                         response = sock2host.recv(buffer)
+                         return response
+                    except socket.gaierror:
+                         print("cann't connect to host: {}".format(host))
+                         response = b'cannot connect to ' + host.encode('utf-8')
+                         return response
+
+          except ConnectionRefusedError:
+               return b'cannot connect to requested host'
+
+     elif type(host) == type(None):
+          return b''
+     
+def handle_conn(conn):
+     data = conn.recv(buffer)
+     print(data)
+     conn.send(RESPONSE)
+     sock_ssh = sock()
+     sock_ssh.connect(('localhost',22))
+     while True:
+          data = conn.recv(buffer)
+          print(data)
+          if data:
+               sock_ssh.send(data)
+               response = sock_ssh.recv(buffer)
+               print('sending respond to client')
+               conn.send(response)
+               continue
 
 class threadforclient(threading.Thread):
      def __init__(self,conn,addr):
           threading.Thread.__init__(self)
           self.conn = conn
           self.ip,self.port = addr
+          self.client = {self.ip:self.conn}
+          self.list =[]
 
      def run(self):
-          data = self.conn.recv(buffer)
-          print(data)
-          self.conn.send(RESPONSE)
-          conn_to_ssh = sock().connect(('localhost',22))
           try:
+               if self.client in self.list:
+                    handle_conn(self.conn)
+               else:
+                    self.list.append(self.client)
                     print("client {} has bieng added".format(self.ip))
-                    while True:
-                         if conn_to_ssh:
-                              data = self.conn.recv(buffer)
-                              print(f'client sent :{data}')
-                              conn_to_ssh.send(data)
-                              response = conn_to_ssh.recv(buffer)
-                              print(f'server responded by {response}')
-                              self.conn.send(response)
-                              continue
-
-                         else:
-                              try:
-                                   conn_to_ssh = sock().connect(('127.0.0.1',22))
-                                   continue
-                              except Exception as e:
-                                   print(e)
-                                   self.conn.close()
-
+                    handle_conn(self.conn) 
           except ConnectionResetError:
-               if conn_to_ssh:
-                    conn_to_ssh.close()
+               if self.client in self.list:
+                    self.list.remove(self.client)
+                    print("connection closed by {}".format(self.ip))
+                    print("client {} has being remove".format(self.ip))
                     self.conn.close()
-               self.conn.close
+                    pass
                     
 def client_handling(socks):
      while True:
@@ -64,6 +119,8 @@ def client_handling(socks):
                print("closing connection of host {}".format(addr))
                conn.close()
                continue
+
+
 def sock_server(port):
      socks = sock()
      socks.bind(("",port))
