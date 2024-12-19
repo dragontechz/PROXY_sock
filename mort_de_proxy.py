4f4cc0c62631dc405723539bb1d0125c97e1f375
dragontechz <juleskono58@gmail.com>
2024-12-19 18:15:19 +0100

    Add files via upload

--- mort_de_proxy.py
+++ mort_de_proxy.py
@@ -6,11 +6,37 @@ import threading
 def sock():
     sock1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     return sock1
+
+class forward():
+    def __init__(self,source,dest):
+        self.source = source
+        self.dest = dest
+
+    def run(self):
+        self.proxy_forward(self.source,self.dest)
+
+    def copy_streams(self, source, destination):
+        try:
+            while True:
+                data = source.recv(2048)
+                if not data:
+                    break
+                destination.sendall(data)
+                continue
+        except Exception as e:
+            print(f"Error copying streams: {e}")
+        finally:
+            source.close()
+            destination.close()
+    def proxy_forward(self,conn,sock_ssh):
+            threading.Thread(target=self.copy_streams, args=(sock_ssh, conn)).start(),
+            threading.Thread(target=self.copy_streams, args=(conn, sock_ssh)).start()
+
 class server():
     def __init__(self,port):
         self.port = port
         self.host = ''
-        self.ssh = ('localhost',22)
+        self.ssh = ('127.0.0.1',22)
     def run(self):
         server_sock = sock()
         server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -27,27 +53,9 @@ class server():
                 print(f'connection initialised by client , ip adress : {ip} forwarding traffic to {self.ssh}')
                 conn.recv(1080)
                 conn.sendall(b'HTTP/1.1 200 OK \r\n\r\n')
-                threading.Thread(target=self.manage_client, args=(sock_ssh, conn)).start()
-                threading.Thread(target=self.manage_client_2, args=(sock_ssh,conn)).start()
-    def copy_streams(self, source, destination):
-        try:
-            while True:
-                data = source.recv(2048)
-                if not data:
-                    break
-                destination.sendall(data)
-        except Exception as e:
-            print(f"Error copying streams: {e}")
-        finally:
-            source.close()
-            destination.close()
-    def manage_client(self,sock_ssh,conn):
-                    threading.Thread(target=self.copy_streams, args=(sock_ssh, conn)).start()
-                    threading.Thread(target=self.copy_streams, args=(conn, sock_ssh)).start()
-                    threading.Thread(target=self.copy_streams, args=(sock_ssh, conn)).start()
-    def manage_client_2(self,sock_ssh,conn):
-                    threading.Thread(target=self.copy_streams, args=(conn, sock_ssh)).start()
-                    threading.Thread(target=self.copy_streams, args=(sock_ssh, conn)).start()
-                    threading.Thread(target=self.copy_streams, args=(conn, sock_ssh)).start()
+                try:
+                    forward(conn,sock_ssh).run()
+                except Exception as e:
+                    print(f'ERROR :{e}')
 if __name__ == "__main__":
     server(9090).run()
